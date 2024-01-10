0fdfcbac6cafd857dcca900e34cc5654fae11abf
Mamun <mdmmn378@gmail.com>
2024-01-10 09:55:46 +0600

    Added cmd parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{ArgAction, Command};
+
+fn generate(val: &clap::ArgMatches) {
+    let path = val.get_one::<String>("path").unwrap();
+    println!("Path: {}", path);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = Command::new("envy").subcommand_required(true);
+    let sub_generate = Command::new("generate")
+        .about("Generate a new .env file")
+        .arg(
+            clap::Arg::new("path")
+                .help("Path to the .env file to generate")
+                .required(true)
+                .action(ArgAction::Set),
+        );
+    cmd = cmd.subcommand(sub_generate);
+    let matches = cmd.get_matches();
+    match matches.subcommand() {
+        Some(("generate", val)) => generate(val),
+        _ => println!("No subcommand"),
+    }
 }
