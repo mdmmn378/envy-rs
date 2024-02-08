e2f5ec19f9f2c33106e817e7c3802d4d2645ede5
Mamun <mdmmn378@gmail.com>
2024-02-08 06:10:50 +0600

    Refactored and separated file format

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
-mod io_manager;
+mod common;
+mod dotenv_handler;
+mod utils;
 
 use clap::{ArgAction, Command};
 use colored::*;
-use io_manager::generate_dot_env_file;
+use common::SupportedFormats;
+use dotenv_handler::generate_dotenv_file;
+use utils::validate_file;
 
 fn generate(val: &clap::ArgMatches) {
     let path = val.get_one::<String>("path").unwrap();
+    let format = validate_file(&path);
     let dry_run: bool = val.get_flag("dry-run");
-    generate_dot_env_file(dry_run, &path).unwrap();
+    match format {
+        SupportedFormats::ENV => {
+            let _ = generate_dotenv_file(dry_run, &path);
+        }
+        _ => {
+            println!(
+                "{} {}",
+                "❌  Error:".red(),
+                "Unsupported file format".bold()
+            );
+            println!(
+                "{} {}",
+                "Supported formats:".bold(),
+                SupportedFormats::supported_formats().join(", ").bold()
+            );
+            std::process::exit(1);
+        }
+    }
     if !dry_run {
         println!(
             "{} {}",
